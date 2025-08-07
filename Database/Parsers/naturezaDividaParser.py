b2760a236058642655dee215b9cfd59ae41426b6
Flavio <77132531+flaviodev27@users.noreply.github.com>
2025-08-06 21:47:56 -0300

    feat: naturezaDividaParser

---
+++ Database/Parsers/naturezaDividaParser.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from App.DTOs.naturezaDividaDTO import NaturezaDividaDTO
+
+from Database.Parsers.baseParser import BaseParser
+
+class NaturezaDividaParser(BaseParser):
+    def __init__(self, path):
+        super().__init__(path)
+
+    def parse(self) -> list:
+        df = self.read_csv()
+
+        naturezaDividaList = []
+
+        for _, row in df.iterrows():
+            obj = NaturezaDividaDTO(
+                id=row['idNaturezadivida'],
+                nome=row['nomnaturezadivida'],
+                descricao=row['descnaturezadivida']
+            )
+            naturezaDividaList.append(obj)
+
+        return naturezaDividaList
+
+
+
